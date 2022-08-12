ec14b01df964cb87e54fde4086e4a10a8aae43ca
Emmanuel Gyan-Amoah <82492796+shakerbillo@users.noreply.github.com>
2022-08-12 08:26:40 +0200

    Add files via upload

---
+++ pactice/src/com/shaker/pactice.java
@@ -0,0 +1,10 @@
+package com.shaker;
+
+
+public class pactice {
+
+    public static void main(String[] args) {
+        double [] arr = new int[2]; //Line 3
+        System.out.println(arr[0]); //Line 4
+    }
+}
